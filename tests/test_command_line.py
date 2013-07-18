7219d9a9ae836742aec4b9d9e128cba1931bd6b9
Forest Gregg <fgregg@uchicago.edu>
2013-07-18 11:34:59 -0500

    refactor dir stucture, fix test

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -5,7 +5,7 @@ import sys
 
 class TestInputAPI(unittest.TestCase) :
     def test_simple(self) :
-        child = pexpect.spawn('python dedupe_csv.py foo bar')
+        child = pexpect.spawn('dedupe foo bar')
         child.expect('error: Could not find the file foo')
 
 
