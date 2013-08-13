7a76c42224b0bd245d165380a5a387ef5b4dcccf
Forest Gregg <fgregg@uchicago.edu>
2013-08-13 17:22:22 -0500

    uniquify input files

--- examples/lobbying/lobbyists.sh
+++ examples/lobbying/lobbyists.sh
@@ -3,18 +3,19 @@
 cat Lobbyists_2012_present.csv | \
 csvpys -s "Full Address" "ch['CLIENT ADDRESS'] + ' ' + ch['CLIENT ADDRESS 2']" | \
 csvpys -s "Client Industry" "ch['CLIENT OTHER INDUSTRY'] if ch['CLIENT INDUSTRY'] == 'OTHER' else ch['CLIENT INDUSTRY']" | \
-sed '1 s/$/,"Award Amount","Vendor ID"/' | \
+sed '1 s/$/,"Vendor ID"/' | \
 sed '2,$s/$/,,/' | \
-csvcut -c "CLIENT NAME","Full Address","CLIENT CITY","CLIENT STATE","CLIENT ZIP","Award Amount","Vendor ID","Client Industry" > \
-lobbyists.csv
-
+csvcut -c "CLIENT NAME","Full Address","CLIENT CITY","CLIENT STATE","CLIENT ZIP","Vendor ID","Client Industry" | \
+csvsort | \
+uniq > lobbyists.csv
 
 cat Contracts_after_8_2010.csv | \
 csvpys -s "Full Address" "ch['Address 1'] + ' ' + ch['Address 2']" | \
 sed '1 s/$/,"Client Industry"/' | \
 sed '2,$s/$/,/' | \
-csvcut -c "Vendor Name","Full Address","City","State","Zip","Award Amount","Vendor ID","Client Industry" > \
-contracts.csv
+csvcut -c "Vendor Name","Full Address","City","State","Zip","Vendor ID","Client Industry" | \
+csvsort | \
+uniq > contracts.csv
 
 csvstack -g lobbyists,contracts -n source lobbyists.csv contracts.csv > stacked.csv
 
