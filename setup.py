2a096270ecd4d23fc8bb70e5fe3e9c64df3325f0
Forest Gregg <fgregg@uchicago.edu>
2014-01-22 16:25:54 -0600

    csvlink is a command

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "csvdedupe",
-    version = '0.0.2',
+    version = '0.1',
     description="Command line tools for deduplicating and merging csv files",
     long_description=open('README.md').read(),
     author="Forest Gregg, Derek Eder",
@@ -11,6 +11,7 @@ setup(
     entry_points ={
         'console_scripts': [
             'csvdedupe = csvdedupe.csvdedupe:launch_new_instance',
+            'csvlink = csvdedupe.csvlink:launch_new_instance'
         ]
     },
     install_requires = [
