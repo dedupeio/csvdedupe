ca9a5a08599dac842eb13f4b8b32c61ae66898d6
Forest Gregg <fgregg@uchicago.edu>
2015-05-23 16:52:12 -0500

    clean formatting of csvdedupe

--- csvdedupe/csvdedupe.py
+++ csvdedupe/csvdedupe.py
@@ -14,8 +14,7 @@ import dedupe
 import itertools
 
 parser = argparse.ArgumentParser(
-  formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 # positional arguments
 parser.add_argument('input', nargs='?', default=sys.stdin,
@@ -40,151 +39,158 @@ parser.add_argument('--destructive', action='store_true',
                     help='Output file will contain unique records only')
 parser.add_argument('-v', '--verbose', action='count', default=0)
 
-class CSVDedupe :
 
-  def __init__(self, args) :
-    configuration = {}
+class CSVDedupe:
+    def __init__(self, args):
+        configuration = {}
 
-    if args.config_file:
-      #read from configuration file
-      try:
-        with open(args.config_file, 'r') as f:
-          config = json.load(f)
-          configuration.update(config)
-      except IOError:
-        raise parser.error("Could not find config file " + args.config_file + '. Did you name it correctly?')
+        if args.config_file:
+            #read from configuration file
+            try:
+                with open(args.config_file, 'r') as f:
+                    config = json.load(f)
+                    configuration.update(config)
+            except IOError:
+                raise parser.error(
+                    "Could not find config file %s. Did you name it correctly?" 
+                    % args.config_file)
 
-    # override if provided from the command line
-    args_d = vars(args)
-    args_d = dict((k,v) for (k,v) in args_d.items() if v is not None)
-    configuration.update(args_d)
+        # override if provided from the command line
+        args_d = vars(args)
+        args_d = dict((k, v) for (k, v) in args_d.items() if v is not None)
+        configuration.update(args_d)
 
-    # set defaults
-    try :
-      # take in STDIN input or open the file
-      if isinstance(configuration['input'], file):
-        if not sys.stdin.isatty() :
-          self.input = configuration['input'].read()
-          # We need to get control of STDIN again.
-          # This is a UNIX/Mac OSX solution only
-          # http://stackoverflow.com/questions/7141331/pipe-input-to-python-program-and-later-get-input-from-user
-          # 
-          # Same question has a Windows solution
-          sys.stdin = open('/dev/tty') # Unix only solution, 
-        else :
-          raise parser.error("No input file or STDIN specified.")
-      else:
-        try :
-          self.input = open(configuration['input'], 'rU').read()
-        except IOError :
-          raise parser.error("Could not find the file %s" % (configuration['input'],))
-    except KeyError :
-          raise parser.error("No input file or STDIN specified.")
+        # set defaults
+        try:
+            # take in STDIN input or open the file
+            if isinstance(configuration['input'], file):
+                if not sys.stdin.isatty():
+                    self.input = configuration['input'].read()
+                    # We need to get control of STDIN again.
+                    # This is a UNIX/Mac OSX solution only
+                    # http://stackoverflow.com/questions/7141331/pipe-input-to-python-program-and-later-get-input-from-user
+                    # 
+                    # Same question has a Windows solution
+                    sys.stdin = open('/dev/tty')  # Unix only solution,
+                else:
+                    raise parser.error("No input file or STDIN specified.")
+            else:
+                try:
+                    self.input = open(configuration['input'], 'rU').read()
+                except IOError:
+                    raise parser.error("Could not find the file %s" %
+                                       (configuration['input'], ))
+        except KeyError:
+            raise parser.error("No input file or STDIN specified.")
 
-    try : 
-      self.field_names = configuration['field_names']
-    except KeyError :
-      raise parser.error("You must provide field_names")
+        try:
+            self.field_names = configuration['field_names']
+        except KeyError:
+            raise parser.error("You must provide field_names")
 
-    self.output_file = configuration.get('output_file', None)
-    self.skip_training = configuration.get('skip_training', False)
-    self.training_file = configuration.get('training_file', 'training.json')
-    self.sample_size = configuration.get('sample_size', 1500)
-    self.recall_weight = configuration.get('recall_weight', 2)
-    self.destructive = configuration.get('destructive', False)
-    
-    if 'field_definition' in configuration :
-      self.field_definition = configuration['field_definition']
-    else :
-      self.field_definition = [{'field' : field, 'type' : 'String'} 
-                               for field in self.field_names]
-      
+        self.output_file = configuration.get('output_file', None)
+        self.skip_training = configuration.get('skip_training', False)
+        self.training_file = configuration.get('training_file',
+                                               'training.json')
+        self.sample_size = configuration.get('sample_size', 1500)
+        self.recall_weight = configuration.get('recall_weight', 2)
+        self.destructive = configuration.get('destructive', False)
 
-  def main(self) :
+        if 'field_definition' in configuration:
+            self.field_definition = configuration['field_definition']
+        else:
+            self.field_definition = [{'field': field,
+                                      'type': 'String'}
+                                     for field in self.field_names]
 
-    data_d = {}
-    # import the specified CSV file
+    def main(self):
 
-    data_d = csvhelpers.readData(self.input, self.field_names)
-    
-    logging.info('imported %d rows', len(data_d))
+        data_d = {}
+        # import the specified CSV file
 
-    # sanity check for provided field names in CSV file
-    for field in self.field_definition :
-      if field['type'] != 'Interaction' :
-        if not field['field'] in data_d[0]:
-        
-          raise parser.error("Could not find field '" + field['field'] + "' in input")
+        data_d = csvhelpers.readData(self.input, self.field_names)
 
-    logging.info('using fields: %s' % [field['field'] for 
-                                       field in self.field_definition])
-    # # Create a new deduper object and pass our data model to it.
-    deduper = dedupe.Dedupe(self.field_definition)
+        logging.info('imported %d rows', len(data_d))
 
-    # Set up our data sample
-    logging.info('taking a sample of %d possible pairs', self.sample_size)
-    deduper.sample(data_d, self.sample_size)
+        # sanity check for provided field names in CSV file
+        for field in self.field_definition:
+            if field['type'] != 'Interaction':
+                if not field['field'] in data_d[0]:
 
-    # If we have training data saved from a previous run of dedupe,
-    # look for it an load it in.
-    # __Note:__ if you want to train from scratch, delete the training_file
+                    raise parser.error("Could not find field '" +
+                                       field['field'] + "' in input")
 
-    if os.path.exists(self.training_file):
-      logging.info('reading labeled examples from %s' % self.training_file)
-      with open(self.training_file) as tf :
-        deduper.readTraining(tf)
-    elif self.skip_training:
-      raise parser.error("You need to provide an existing training_file or run this script without --skip_training")
+        logging.info('using fields: %s' % [field['field']
+                                           for field in self.field_definition])
+        # # Create a new deduper object and pass our data model to it.
+        deduper = dedupe.Dedupe(self.field_definition)
 
-    if not self.skip_training:
-      logging.info('starting active labeling...')
+        # Set up our data sample
+        logging.info('taking a sample of %d possible pairs', self.sample_size)
+        deduper.sample(data_d, self.sample_size)
 
-      csvhelpers.consoleLabel(deduper)
+        # If we have training data saved from a previous run of dedupe,
+        # look for it an load it in.
+        # __Note:__ if you want to train from scratch, delete the training_file
 
-      # When finished, save our training away to disk
-      logging.info('saving training data to %s' % self.training_file)
-      with open(self.training_file, 'w') as tf :
-        deduper.writeTraining(tf)
-    else:
-      logging.info('skipping the training step')
-      
-    deduper.train()
+        if os.path.exists(self.training_file):
+            logging.info('reading labeled examples from %s' %
+                         self.training_file)
+            with open(self.training_file) as tf:
+                deduper.readTraining(tf)
+        elif self.skip_training:
+            raise parser.error(
+                "You need to provide an existing training_file or run this script without --skip_training")
 
-    # ## Blocking
+        if not self.skip_training:
+            logging.info('starting active labeling...')
 
-    logging.info('blocking...')
+            dedupe.consoleLabel(deduper)
 
-    # ## Clustering
+            # When finished, save our training away to disk
+            logging.info('saving training data to %s' % self.training_file)
+            with open(self.training_file, 'w') as tf:
+                deduper.writeTraining(tf)
+        else:
+            logging.info('skipping the training step')
 
-    # Find the threshold that will maximize a weighted average of our precision and recall. 
-    # When we set the recall weight to 2, we are saying we care twice as much
-    # about recall as we do precision.
-    #
-    # If we had more data, we would not pass in all the blocked data into
-    # this function but a representative sample.
+        deduper.train()
 
-    logging.info('finding a good threshold with a recall_weight of %s' % 
-                 self.recall_weight)
-    threshold = deduper.threshold(data_d, recall_weight=self.recall_weight)
+        # ## Blocking
 
-    # `duplicateClusters` will return sets of record IDs that dedupe
-    # believes are all referring to the same entity.
+        logging.info('blocking...')
 
-    logging.info('clustering...')
-    clustered_dupes = deduper.match(data_d, threshold)
+        # ## Clustering
 
-    logging.info('# duplicate sets %s' % len(clustered_dupes))
+        # Find the threshold that will maximize a weighted average of our precision and recall. 
+        # When we set the recall weight to 2, we are saying we care twice as much
+        # about recall as we do precision.
+        #
+        # If we had more data, we would not pass in all the blocked data into
+        # this function but a representative sample.
 
-    write_function = csvhelpers.writeResults
-    # write out our results
-    if self.destructive:
-      write_function = csvhelpers.writeUniqueResults
+        logging.info('finding a good threshold with a recall_weight of %s' %
+                     self.recall_weight)
+        threshold = deduper.threshold(data_d, recall_weight=self.recall_weight)
 
-    if self.output_file :
-      with open(self.output_file, 'w') as output_file :
-        write_function(clustered_dupes, self.input, output_file)
-    else :
-        write_function(clustered_dupes, self.input, sys.stdout)
+        # `duplicateClusters` will return sets of record IDs that dedupe
+        # believes are all referring to the same entity.
+
+        logging.info('clustering...')
+        clustered_dupes = deduper.match(data_d, threshold)
+
+        logging.info('# duplicate sets %s' % len(clustered_dupes))
+
+        write_function = csvhelpers.writeResults
+        # write out our results
+        if self.destructive:
+            write_function = csvhelpers.writeUniqueResults
+
+        if self.output_file:
+            with open(self.output_file, 'w') as output_file:
+                write_function(clustered_dupes, self.input, output_file)
+        else:
+            write_function(clustered_dupes, self.input, sys.stdout)
 
 
 def launch_new_instance():
@@ -192,13 +198,14 @@ def launch_new_instance():
 
     log_level = logging.WARNING
     if args.verbose == 1:
-      log_level = logging.INFO
+        log_level = logging.INFO
     elif args.verbose >= 2:
-      log_level = logging.DEBUG
+        log_level = logging.DEBUG
     logging.basicConfig(level=log_level)
 
     d = CSVDedupe(args)
     d.main()
-    
+
+
 if __name__ == "__main__":
     launch_new_instance()
