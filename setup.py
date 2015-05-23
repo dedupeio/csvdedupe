aead866682f7bc03db3889b1930024261d14d670
Forest Gregg <fgregg@uchicago.edu>
2015-05-23 18:21:32 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "csvdedupe",
-    version = '0.1.6',
+    version = '0.1.7',
     description="Command line tools for deduplicating and merging csv files",
     author="Forest Gregg, Derek Eder",
     license="MIT",
