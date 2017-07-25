c4a961e56f692e98f23a626f135cf6fed2482423
Jaroslav Kostal <jaroslav@kostal.sk>
2017-07-25 22:31:17 +0200

    Use utf-8 to read input files in csvlink [closes #72]

--- csvdedupe/csvlink.py
+++ csvdedupe/csvlink.py
@@ -18,13 +18,13 @@ class CSVLink(csvhelpers.CSVCommand):
 
         if len(self.configuration['input']) == 2:
             try:
-                self.input_1 = open(self.configuration['input'][0], 'rU').read()
+                self.input_1 = open(self.configuration['input'][0], encoding='utf-8').read()
             except IOError:
                 raise self.parser.error("Could not find the file %s" %
                                    (self.configuration['input'][0], ))
 
             try:
-                self.input_2 = open(self.configuration['input'][1], 'rU').read()
+                self.input_2 = open(self.configuration['input'][1], encoding='utf-8').read()
             except IOError:
                 raise self.parser.error("Could not find the file %s" %
                                    (self.configuration['input'][1], ))
