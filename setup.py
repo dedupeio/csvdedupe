b0bf8de37a2f15e82a7d9a07c15409cf9717a71a
Forest Gregg <fgregg@uchicago.edu>
2020-03-07 14:11:28 -0500

    include readme

--- setup.py
+++ setup.py
@@ -7,9 +7,15 @@ requirements = ['future>=0.14',
 if sys.version < '3':
     requirements += ['backports.csv']
 
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = "csvdedupe",
-    version = '0.1.19',
+    version = '0.1.20',
     description="Command line tools for deduplicating and merging csv files",
     author="Forest Gregg, Derek Eder",
     license="MIT",
@@ -20,5 +26,7 @@ setup(
             'csvlink = csvdedupe.csvlink:launch_new_instance'
         ]
     },
-    install_requires = requirements
+    install_requires = requirements,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
